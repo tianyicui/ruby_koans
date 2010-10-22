48ff325ed2ab8c560c56496fe1b69dd4f588f752
Tianyi Cui <tianyicui@gmail.com>
2010-10-22 13:52:14 +0800

    triangle

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  {
+    3 => :equilateral,
+    1 => :isosceles,
+    0 => :scalene
+  }[ [a==b, b==c, c==a].count(true) ]
 end
 
 # Error class used in part 2.  No need to change this code.

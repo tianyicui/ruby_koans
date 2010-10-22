a270800ece3fd1fc5fa4d8348c62fab34ad2ad2e
Tianyi Cui <tianyicui@gmail.com>
2010-10-22 14:01:37 +0800

    triangle 2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,11 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+   a, b, c = [a, b, c].sort
+   raise TriangleError if a<=0
+   raise TriangleError if a+b<=c
   {
-    3 => :equilateral,
+    2 => :equilateral,
     1 => :isosceles,
     0 => :scalene
-  }[ [a==b, b==c, c==a].count(true) ]
+  }[ [a==b, b==c].count(true) ]
 end
 
 # Error class used in part 2.  No need to change this code.
